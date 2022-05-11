82f44239b6059c76a0e96d717818461a6f665511
Amy <llamanozz@gmail.com>
2022-05-11 12:27:38 +0200

    Add remaining page parsing logic

--- page.py
+++ page.py
@@ -6,22 +6,22 @@ import requests
 import urllib.parse
 import re
 
-# app = Flask(__name__)
-# cors = CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def remove_refs(input):
-  # removes references such as [1]
+  # remove references in square brackets such as [1]
   input = re.sub('[\[0-9\]]', '', input)
   
-  # removes the escape backslash before a single quote
+  # remove the escape backslash before a single quote
   input = re.sub('[\\\']', '\'', input)
-  
+
   return input
 
-# @app.route('/page/', methods=['GET'])
-def page():
-  URL = "https://en.wikipedia.org/wiki/Ancient_Greek_philosophy"
+@app.route('/page/<title>', methods=['GET'])
+def page(title):
+  URL = "https://en.wikipedia.org/wiki/%s" % title
   page = requests.get(URL)
   soup = BeautifulSoup(page.content, "lxml")
 
@@ -39,10 +39,32 @@ def page():
       # add the tag plus its text in reverse order to page_tags
       page_tags.insert(0, [sibling.name, inner_text])
 
-  
-  print(page_tags)
+  # for all remaining p, h2, h3, h4 and ul tags after the table of contents
+  for tag in soup.find(id="toc").next_siblings:
+    if tag.name == "p" or tag.name == "h2" or tag.name == "h3" or tag.name == "h4":
+      inner_text = remove_refs(tag.text.strip())
+
+      # the last 4 chars of h2, h3 and h4 tags are typically "edit"
+      if inner_text[-4:] == "edit":
+        inner_text = inner_text[:-4]
+
+      # The article should end at the "See also" heading
+      if inner_text == "See also":
+        break
+
+      page_tags.append([tag.name, inner_text])
+
+    if tag.name == "ul":
+      ul_contents = []
+      for li_tag in tag:
+        li_tag = li_tag.text.strip()
+        if len(li_tag) != 0:
+          ul_contents.append(li_tag)
+      page_tags.append([tag.name, ul_contents])
+
+  return jsonify(page_tags)
+
 
 
 if __name__ == '__main__':
-  # app.run()
-  page()
+  app.run()
